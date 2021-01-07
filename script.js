c176b05ed9fb27cae39b73a36c68a2f9c41b901a
AryanMiddlagajani <middlagajaniaryan@gmail.com>
2021-01-07 16:07:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var counter = document.querySelector(".counter");
+var followers = document.querySelector(".followers");
+
+counter.innerHTML = 1001;
+
+var counter = document.querySelector(".counter");
+var followers = document.querySelector(".followers");
+let count = 1;
+setInterval(() => {
+  count++;
+  counter.innerText = count;
+}, 1000);
+
+var counter = document.querySelector(".counter");
+var followers = document.querySelector(".followers");
+let count = 1;
+
+setInterval(() => {
+  if (count < 1000) {
+    count++;
+    counter.innerText = count;
+  }
+}, 1);
+setTimeout(() => {
+  followers.innerText = "Followers in Instagram!";
+}, 5000);
